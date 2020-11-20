26d176a6771b5d7a4cb34b2e500f297ee22d7393
Sharon-liu-go <innovate72095@gmail.com>
2020-11-20 14:59:49 +0800

    feat : add login and register pages, and add router modules

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebar' }))
-app.set('view engine', 'handlebar')
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
+app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
 
 
 
